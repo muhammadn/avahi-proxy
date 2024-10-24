9c226d4c8231c7c1a54a993f47681008fbd7f037
Muhammad Nuzaihan Bin Kamal Luddin <muhammad@taqisystems.com>
2024-10-25 05:21:30 +0800

    Initial commit.
    This is at least working to fill my needs. Will add options to set your own local network domains
    to be mapped to a local Multicast DNS resolver (avahi)

---
+++ cmd/avahi-proxy/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+
+	"github.com/godbus/dbus/v5"
+	"github.com/holoplot/go-avahi"
+	"github.com/miekg/dns"
+	"github.com/spf13/cobra"
+)
+
+func handleDnsRequest(w dns.ResponseWriter, r *dns.Msg) {
+	m := new(dns.Msg)
+	m.SetReply(r)
+	m.Compress = false
+
+	switch r.Opcode {
+	case dns.OpcodeQuery:
+		parseQuery(m)
+	}
+
+	w.WriteMsg(m)
+}
+
+func mDNSResolver(fqdn string, rrtype int32) avahi.HostName {
+	// split the host.domain.local and get the host string only
+        spltstr := strings.Split(fqdn, ".")
+	host := spltstr[0]
+	fqdn = host + ".local"
+
+        conn, err := dbus.SystemBus()
+        if err != nil {
+                log.Fatalf("Cannot get system bus: %v", err)
+        }
+
+        server, err := avahi.ServerNew(conn)
+        if err != nil {
+                log.Fatalf("Avahi new failed: %v", err)
+        }
+
+        hn, err := server.ResolveHostName(avahi.InterfaceUnspec, rrtype, fqdn, rrtype, 0)
+        if err != nil {
+                log.Println("ResolveHostName() failed: %v", err)
+        }
+        log.Println("ResolveHostName:", hn.Address)
+
+	return hn
+}
+
+func parseQuery(m *dns.Msg) {
+	for _, q := range m.Question {
+		switch q.Qtype {
+		case dns.TypeA:
+			log.Printf("IPv4 query for %s\n", q.Name)
+			result := mDNSResolver(q.Name, 0) // 0 integer is ProtoInet (see go-avahi source types.go) 
+			ip := result.Address
+			if ip != "" {
+				rr, err := dns.NewRR(fmt.Sprintf("%s A %s", q.Name, ip))
+				if err == nil {
+					m.Answer = append(m.Answer, rr)
+				}
+			}
+		case dns.TypeAAAA:
+                        log.Printf("IPv6 query for %s\n", q.Name)
+                        result := mDNSResolver(q.Name, 1) // 1 integer is ProtoInet (see go-avahi source types.go) 
+                        ip := result.Address
+                        if ip != "" {
+                                rr, err := dns.NewRR(fmt.Sprintf("%s AAAA %s", q.Name, ip))
+                                if err == nil {
+                                        m.Answer = append(m.Answer, rr)
+                                }
+                        }
+		}
+	}
+}
+
+func main() {
+	// attach request handler func
+	dns.HandleFunc("home.lan.", handleDnsRequest)
+
+	// start server
+	port := 5354
+	server := &dns.Server{Addr: ":" + strconv.Itoa(port), Net: "udp"}
+	log.Printf("Starting at %d\n", port)
+	err = server.ListenAndServe()
+	defer server.Shutdown()
+	if err != nil {
+		log.Fatalf("Failed to start server: %s\n ", err.Error())
+	}
+
+}
